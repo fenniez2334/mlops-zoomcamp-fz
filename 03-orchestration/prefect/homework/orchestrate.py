c085d875efbe70521cf5a09590246eac3c658353
Feifei Zhao <77315830+fenniez2334@users.noreply.github.com>
2025-06-05 05:33:52 +0000

    homework solutions updates

--- 03-orchestration/prefect/homework/orchestrate.py
+++ 03-orchestration/prefect/homework/orchestrate.py
@@ -5,10 +5,11 @@ import numpy as np
 import scipy
 import sklearn
 from sklearn.feature_extraction import DictVectorizer
-from sklearn.metrics import root_mean_squared_error
+from sklearn.linear_model import LinearRegression
 import mlflow
-import xgboost as xgb
+from typing import Tuple
 from prefect import flow, task
+from scipy.sparse import csr_matrix
 
 
 @task(retries=3, retry_delay_seconds=2)
@@ -16,10 +17,12 @@ def read_data(filename: str) -> pd.DataFrame:
     """Read data into DataFrame"""
     df = pd.read_parquet(filename)
 
-    df.lpep_dropoff_datetime = pd.to_datetime(df.lpep_dropoff_datetime)
-    df.lpep_pickup_datetime = pd.to_datetime(df.lpep_pickup_datetime)
+    print(f"Read {len(df)} records from {filename}")
 
-    df["duration"] = df.lpep_dropoff_datetime - df.lpep_pickup_datetime
+    # df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+    # df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+
+    df["duration"] = df.tpep_dropoff_datetime - df.tpep_pickup_datetime
     df.duration = df.duration.apply(lambda td: td.total_seconds() / 60)
 
     df = df[(df.duration >= 1) & (df.duration <= 60)]
@@ -27,26 +30,20 @@ def read_data(filename: str) -> pd.DataFrame:
     categorical = ["PULocationID", "DOLocationID"]
     df[categorical] = df[categorical].astype(str)
 
+    print(f"Adjusted dataframe has {len(df)} records.")
+
     return df
 
 
 @task
 def add_features(
-    df_train: pd.DataFrame, df_val: pd.DataFrame
-) -> tuple(
-    [
-        scipy.sparse._csr.csr_matrix,
-        scipy.sparse._csr.csr_matrix,
-        np.ndarray,
-        np.ndarray,
-        sklearn.feature_extraction.DictVectorizer,
-    ]
-):
+    df_train: pd.DataFrame,
+) -> Tuple[csr_matrix, np.ndarray, DictVectorizer]:
     """Add features to the model"""
-    df_train["PU_DO"] = df_train["PULocationID"] + "_" + df_train["DOLocationID"]
-    df_val["PU_DO"] = df_val["PULocationID"] + "_" + df_val["DOLocationID"]
+    # df_train["PU_DO"] = df_train["PULocationID"] + "_" + df_train["DOLocationID"]
+    # df_val["PU_DO"] = df_val["PULocationID"] + "_" + df_val["DOLocationID"]
 
-    categorical = ["PU_DO"]  #'PULocationID', 'DOLocationID']
+    categorical = ['PULocationID', 'DOLocationID']
     numerical = ["trip_distance"]
 
     dv = DictVectorizer()
@@ -54,65 +51,46 @@ def add_features(
     train_dicts = df_train[categorical + numerical].to_dict(orient="records")
     X_train = dv.fit_transform(train_dicts)
 
-    val_dicts = df_val[categorical + numerical].to_dict(orient="records")
-    X_val = dv.transform(val_dicts)
+    # val_dicts = df_val[categorical + numerical].to_dict(orient="records")
+    # X_val = dv.transform(val_dicts)
 
     y_train = df_train["duration"].values
-    y_val = df_val["duration"].values
-    return X_train, X_val, y_train, y_val, dv
+    # y_val = df_val["duration"].values
+    return X_train, y_train, dv
 
 
 @task(log_prints=True)
 def train_best_model(
     X_train: scipy.sparse._csr.csr_matrix,
-    X_val: scipy.sparse._csr.csr_matrix,
+    # X_val: scipy.sparse._csr.csr_matrix,
     y_train: np.ndarray,
-    y_val: np.ndarray,
+    # y_val: np.ndarray,
     dv: sklearn.feature_extraction.DictVectorizer,
 ) -> None:
-    """train a model with best hyperparams and write everything out"""
+    """Train a linear regression model and log artifacts"""
 
     with mlflow.start_run():
-        train = xgb.DMatrix(X_train, label=y_train)
-        valid = xgb.DMatrix(X_val, label=y_val)
 
-        best_params = {
-            "learning_rate": 0.09585355369315604,
-            "max_depth": 30,
-            "min_child_weight": 1.060597050922164,
-            "objective": "reg:linear",
-            "reg_alpha": 0.018060244040060163,
-            "reg_lambda": 0.011658731377413597,
-            "seed": 42,
-        }
+        mlflow.log_param("train-data-path", "./data/yellow_tripdata_2023-03.parquet")
 
-        mlflow.log_params(best_params)
+        lr = LinearRegression()
+        lr.fit(X_train, y_train)
 
-        booster = xgb.train(
-            params=best_params,
-            dtrain=train,
-            num_boost_round=10,
-            evals=[(valid, "validation")],
-            early_stopping_rounds=20,
-        )
-
-        y_pred = booster.predict(valid)
-        rmse = root_mean_squared_error(y_val, y_pred)
-        mlflow.log_metric("rmse", rmse)
+        print("Intercept:", lr.intercept_)
 
         pathlib.Path("models").mkdir(exist_ok=True)
-        with open("models/preprocessor.b", "wb") as f_out:
-            pickle.dump(dv, f_out)
-        mlflow.log_artifact("models/preprocessor.b", artifact_path="preprocessor")
+        with open("models/lin_reg.bin", "wb") as f_out:
+            pickle.dump((dv, lr), f_out)
 
-        mlflow.xgboost.log_model(booster, artifact_path="models_mlflow")
+        mlflow.log_artifact("models/lin_reg.bin", artifact_path="lin_reg")
+        mlflow.sklearn.log_model(lr, artifact_path="models_mlflow")
     return None
 
 
 @flow
 def main_flow(
-    train_path: str = "./data/green_tripdata_2023-01.parquet",
-    val_path: str = "./data/green_tripdata_2023-02.parquet",
+    train_path: str = "./data/yellow_tripdata_2023-03.parquet",
+    # val_path: str = "./data/green_tripdata_2023-02.parquet",
 ) -> None:
     """The main training pipeline"""
 
@@ -122,13 +100,13 @@ def main_flow(
 
     # Load
     df_train = read_data(train_path)
-    df_val = read_data(val_path)
+    # df_val = read_data(val_path)
 
     # Transform
-    X_train, X_val, y_train, y_val, dv = add_features(df_train, df_val)
+    X_train, y_train, dv = add_features(df_train)
 
     # Train
-    train_best_model(X_train, X_val, y_train, y_val, dv)
+    train_best_model(X_train, y_train, dv)
 
 
 if __name__ == "__main__":
